393c624103e30fbba3519d9d1fcd1b880ea84310
JJAYSF <james.jay@snowflake.com>
2022-07-26 14:08:36 -0700

    Lesson 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,31 @@
 
+#testing
+
+import streamlit 
+import pandas
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('🥑Breakfast Menu')
+streamlit.text('🥑Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥑Kale....')
+streamlit.text('🥑Head-Boiled...)
+               
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+               
+# interactions 
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+               
+streamlit.dataframe =(fruits_to_show)
+
+# Display the table on the page.
+               
+# pull data 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# sets the fruit list to display in menu
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# display data
+streamlit.dataframe(my_fruit_list)
